e0fbccd6575f41e21f726fb4b3ce8177c0948764
KipzonderKop101 <lodewijkboon@icloud.com>
2023-03-02 12:27:30 +0100

    Based for login

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Crypto App',
+      theme: ThemeData(
+      primarySwatch: : Colors.blue,),
+      home: LoginScreen(),
+    );
+  }
+}
